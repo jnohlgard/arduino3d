d769b658f6bcdfd6f6010cba87f0d4da79942bf6
Joakim Gebart <joakim.gebart@jge.se>
2012-04-08 12:21:59 +0200

    utility3d: namespace in header file.

--- utility3d.hpp
+++ utility3d.hpp
@@ -5,42 +5,42 @@
 namespace A3D
 {
 
-void Frustum(Array::Matrix4f& matrix, const float left, const float right, const float bottom, const float top, const float near, const float far);
+void Frustum(Matrix4f& matrix, const float left, const float right, const float bottom, const float top, const float near, const float far);
 
-void Diagonal(Array::Matrix4f& matrix, const float scale);
+void Diagonal(Matrix4f& matrix, const float scale);
 
-inline void Identity(Array::Matrix4f& matrix)
+inline void Identity(Matrix4f& matrix)
 {
     Diagonal(matrix, 1.0f);
 }
 
-void RotateXrad(Array::Matrix4f& matrix, const float radians);
-void RotateYrad(Array::Matrix4f& matrix, const float radians);
-void RotateZrad(Array::Matrix4f& matrix, const float radians);
+void RotateXrad(Matrix4f& matrix, const float radians);
+void RotateYrad(Matrix4f& matrix, const float radians);
+void RotateZrad(Matrix4f& matrix, const float radians);
 
-inline void RotateXdeg(Array::Matrix4f& matrix, const float degrees)
+inline void RotateXdeg(Matrix4f& matrix, const float degrees)
 {
     RotateXrad(matrix, degrees * (M_PI/180.0f));
 }
 
-inline void RotateYdeg(Array::Matrix4f& matrix, const float degrees)
+inline void RotateYdeg(Matrix4f& matrix, const float degrees)
 {
     RotateYrad(matrix, degrees * (M_PI/180.0f));
 }
 
-inline void RotateZdeg(Array::Matrix4f& matrix, const float degrees)
+inline void RotateZdeg(Matrix4f& matrix, const float degrees)
 {
     RotateZrad(matrix, degrees * (M_PI/180.0f));
 }
 
-void Scale(Array::Matrix4f& matrix, const float scale_x, const float scale_y, const float scale_z);
+void Scale(Matrix4f& matrix, const float scale_x, const float scale_y, const float scale_z);
 
-inline void Scale(Array::Matrix4f& matrix, const float scale)
+inline void Scale(Matrix4f& matrix, const float scale)
 {
     Scale(matrix, scale, scale, scale);
 }
 
-void Translate(Array::Matrix4f& matrix, const float dx, const float dy, const float dz);
+void Translate(Matrix4f& matrix, const float dx, const float dy, const float dz);
 
 }
 #endif
